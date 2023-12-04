7a3377f4a847843a9596987664bd2d00f4cf792b
Igor Fernandes <igorfercontato@gmail.com>
2023-12-04 17:03:59 -0300

    Aula 7

--- scenes/game_camera/game_camera.gd
+++ scenes/game_camera/game_camera.gd
@@ -2,15 +2,14 @@ extends Camera2D
 
 var target_position = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	make_current()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	acquire_target()
 	global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 2))
+
 
 func acquire_target():
 	var player_nodes = get_tree().get_nodes_in_group("player")
