4e834916a6a9d93b495e8100c82000c970246597
Igor Fernandes <igorfercontato@gmail.com>
2023-12-17 00:06:21 -0300

    Aula 29

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,11 +1,22 @@
 extends Node
 
-@export var victory_screen_scene: PackedScene
+const DIFFICULTY_INTERVAL = 5
+
+@export var end_screen_scene: PackedScene
+
 @onready var timer = $Timer
+
+var arena_difficulty = 0
+var previous_time = 0
 
 
 func _ready():
+	previous_time = timer.wait_time
 	timer.timeout.connect(on_timer_timeout)
+
+
+func _process(delta):
+	pass
 
 
 func get_time_elapsed():
@@ -13,5 +24,5 @@ func get_time_elapsed():
 
 
 func on_timer_timeout():
-	var victory_screen_instance = victory_screen_scene.instantiate()
-	add_child(victory_screen_instance)
+	var end_screen_instance = end_screen_scene.instantiate()
+	add_child(end_screen_instance)
