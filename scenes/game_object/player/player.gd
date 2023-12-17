e0e3f3a5d21ee9b5cbfb898c2daff57c4271a84e
Igor Fernandes <igorfercontato@gmail.com>
2023-12-16 21:59:59 -0300

    Aula 27

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -1,19 +1,21 @@
 extends CharacterBody2D
 
 @onready var damage_interval_timer = $DamageIntervalTimer
+@onready var health_component = $HealthComponent as HealthComponent
+@onready var health_bar = $HealthBar
 
 const MAX_SPEED = 125
 const ACCELERATION_SMOOTHING = 25
 
-
 var number_colliding_bodies = 0
-
 
 
 func _ready():
 	$CollisionArea2D.body_entered.connect(on_body_entered)
 	$CollisionArea2D.body_exited.connect(on_body_exited)
 	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
+	health_component.health_changed.connect(on_health_changed)
+	update_health_display()
 
 
 func _process(delta):
@@ -35,8 +37,13 @@ func get_movement_vector():
 func check_deal_damage():
 	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
 		return
-	$HealthComponent.damage(1)
+	health_component.damage(1)
 	damage_interval_timer.start()
+	print(health_component.current_health)
+
+
+func update_health_display():
+	health_bar.value = health_component.get_health_percent()
 
 
 func on_body_entered(other_body: Node2D):
@@ -50,3 +57,7 @@ func on_body_exited(other_body: Node2D):
 
 func on_damage_interval_timer_timeout():
 	check_deal_damage()
+
+
+func on_health_changed():
+	update_health_display()
